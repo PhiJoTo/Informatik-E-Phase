21df8543d71f37ff9ebc755da2f34d695682672e
Philipp <p.troost04@gmail.com>
2022-02-24 18:08:44 +0100

    Aufgabe 13 und 14 fixed

--- Aufgaben - Java/Aufgabe13u14.java
+++ Aufgaben - Java/Aufgabe13u14.java
@@ -1,13 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Aufgabe13u14 {
+
+  static Scanner keyboard = new Scanner(System.in);
+
   public static void main(String[] args) {
-    System.out.println(maxNumber(int[]));
+    // array of ints
+    List<Integer> numberArray = new ArrayList<Integer>();
+    // keyboard input
+    String inputString = keyboard.nextLine();
+    // split input string into array of strings
+    String[] inputArray = inputString.split(" ");
+    // convert array of strings to array of ints
+    for (int i = 0; i < inputArray.length; i++) {
+      numberArray.add(Integer.parseInt(inputArray[i]));
+    }
+
+    System.out.println(maxNumber(numberArray));
   }
 
-  public static int maxNumber(int[] numbers) {
+  public static int maxNumber(List<Integer> numbers) {
     int maxNumber = 0;
     for (int number : numbers) {
       maxNumber = maxNumber < number ? number : maxNumber;
-
     }
 
     return maxNumber;
