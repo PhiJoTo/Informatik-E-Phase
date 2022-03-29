f6d075bb5075b9288ece00560252a41ede88d521
Philipp <p.troost04@gmail.com>
2022-03-29 08:27:02 +0200

    sd

---
+++ Aufgaben - Java/Aufgabe20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Aufgabe20 {
+  public static void main(String[] args) {
+    float x= 1;
+    float a= 1;
+    float e=2;
+    float s=1;
+    float y = 0;
+    System.out.print("Gib den Anfangswert ein, ab dem die Wertetabelle ausgegeben werden soll: ");
+    try (Scanner keyboard = new Scanner(System.in)) {
+      a = keyboard.nextFloat();
+      System.out.print("Gib den Endwert ein, bis zu dem die Wertetabelle ausgegeben werden soll: ");
+      e = keyboard.nextFloat();
+      System.out.print("Gib den Schrittweite ein: ");
+      s = keyboard.nextFloat();
+    }
+    // calculating the numbers
+    for (x = a; x <= e; x += s) {
+      y = ((3*x) * (3*x))-8*x + 4;
+      System.out.println(x + " " + y);
+    }
+  }
+}
---
+++ Aufgaben - Java/Aufgabe21.java
@@ -0,0 +1,13 @@
+public class Aufgabe21 {
+  public static void main(String[] args) {
+    //Vermutung: Die untere SChleife wird immer öfter ausgeführt bis 5
+
+    //Überprüfung:
+      for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+  }
+}
